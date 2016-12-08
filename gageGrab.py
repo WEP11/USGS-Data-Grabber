1bba4af44f0596432ecc49c10780f869d0240da5
Warren Pettee <Warren Pettee>
2016-12-08 16:04:37 -0500

    Changes to be committed: 	new file:   gageGrab.py
     Changes not staged for commit:
    	modified:   README.md

---
+++ gageGrab.py
@@ -0,0 +1,70 @@
+##################################################
+#                                                #
+#              USGS Data Grabber                 #
+#         GeoReferenced Station Creator          #
+#       Author: Warren Pettee (@wpettee)         #
+#                                                #
+##################################################
+import requests
+import urllib
+from urllib.request import urlopen
+import xml.etree.ElementTree as etree
+from pykml.factory import KML_ElementMaker as KML
+
+stationTypeDict = {'AG' : 'Aggregate groundwater use', 'AS' : 'Aggregate surface-water-use',
+                'AT' : 'Atmosphere', 'AW' : 'Aggregate water-use establishment',
+                'ES' : 'Estuary', 'FA' : 'Facility', 'FA-AWL' : 'Animal waste lagoon',
+                'FA-CI' : 'Cistern', 'FA-CS' : 'Combined Sewer', 'FA-DV' : 'Diversion',
+                'FA-FON' : 'Field, Pasture, Orchard, or Nursery', 'FA-GC' : 'Golf Course',
+                'FA-HP' : 'Hydroelectric Plant', 'FA-LF' : 'Landfill', 'FA-OF' : 'Outfall',
+                'FA-PV' : 'Pavement', 'FA-QC' : 'Lab or Sample Prep Area', 'FA-SEW' : 'Wastewater Sewer',
+                'FA-SPS' : 'Septic System', 'FA-STS' : 'Storm Sewer', 'FA-TEP' : 'Thermoelectric Plant',
+                'FA-WDS' : 'Water Distribution System', 'FA-WIW' : 'Waste Injection Well',
+                'FA-WTP' : 'Water-supply Treatment Plant', 'FA-WU' : 'Water Use Establishment',
+                'FA-WWD' : 'Wastewater land application', 'FA-WWTP' : 'Wastewater Treatment Plant',
+                'GL' : 'Glacier', 'GW' : 'Well', 'GW-CR' : 'Collector or Ranney type well',
+                'GW-EX' : 'Extensometer Well', 'GW-HZ' : 'Hyporheic-zone Well',
+                'GW-IW' : 'Interconnected Well', 'MW' : 'Multiple Wells', 'GW-TH' : 'Test Hole',
+                'LA' : 'Land', 'LA-EX' : 'Excavation', 'LA-OU' : 'Outcrop', 'LA-PLY' : 'Playa',
+                'LA-SH' : 'Soil Hole', 'LA-SNK' : 'Sinkhole', 'LA-SR' : 'Shore', 'LA-VOL' : 'Volcanic Vent',
+                'LK' : 'Lake', 'OC' : 'Ocean', 'OC-CO' : 'Coastal', 'SB' : 'Subsurface', 'SB-CV' : 'Cave',
+                'SB-GWD' : 'Groundwater Drain', 'SB-TSM' : 'Tunnel', 'SB-UZ' : 'Unsaturated Zone',
+                'SP' : 'Spring', 'SS' : 'Specific Source', 'ST' : 'Stream', 'ST-CA' : 'Canal',
+                'ST-DCH' : 'Ditch', 'ST-TS' : 'Tidal Stream', 'WE' : 'Wetland'}
+
+stationName = []
+stationLat = []
+stationLon = []
+stationType = []
+gageHeight = []
+
+response = urllib.request.urlopen("http://waterservices.usgs.gov/nwis/iv/?countyCd=37119")
+tree = etree.parse(response)
+root = tree.getroot()
+
+i=0
+tempName=' '
+for child in root:
+    if i >= 1 :
+        if tempName != root[i][0][0].text:
+            print()
+            tempName = root[i][0][0].text
+        stationName.append(root[i][0][0].text)
+        stationLat.append(root[i][0][3][0][0].text)
+        stationLon.append(root[i][0][3][0][0].text)
+        stationType.append(root[i][0][4].text)
+        if root[i][1][0].get("variableID") == '45807197' :
+                print(stationName[i-1], ' Streamflow: ', root[i][2][0].text, ' ',root[i][2][0].get("dateTime"))
+        if root[i][1][0].get("variableID") == '45807202' :
+                print(stationName[i-1], ' Gageheight: ', root[i][2][0].text, ' ',root[i][2][0].get("dateTime"))
+        if root[i][1][0].get("variableID") == '45807140' :
+                print(stationName[i-1], ' Rainfall: ', root[i][2][0].text, ' ',root[i][2][0].get("dateTime"))
+    i = i+1
+
+# First we accept a station info tsv as input...
+
+# Now we organize the data...
+
+# Now we ID what kind of stations these are...
+
+# Finally, we spit this out as either geoJSON, KML, or SHP...
