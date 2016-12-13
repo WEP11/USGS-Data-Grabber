271bf46e680cad86ac03eca7179d43132343f284
WEP11 <wepettee@gmail.com>
2016-12-12 21:44:12 -0500

    Changes to be committed: 	modified:   gageGrab.py

--- gageGrab.py
+++ gageGrab.py
@@ -12,47 +12,37 @@ import requests
 import urllib
 from urllib.request import urlopen
 import xml.etree.ElementTree as etree
-
+import pandas
+import usgs
 #------------------------------------------------
-
-# Listing of all station types :
-stationTypeDict = {'AG' : 'Aggregate groundwater use', 'AS' : 'Aggregate surface-water-use',
-                'AT' : 'Atmosphere', 'AW' : 'Aggregate water-use establishment',
-                'ES' : 'Estuary', 'FA' : 'Facility', 'FA-AWL' : 'Animal waste lagoon',
-                'FA-CI' : 'Cistern', 'FA-CS' : 'Combined Sewer', 'FA-DV' : 'Diversion',
-                'FA-FON' : 'Field, Pasture, Orchard, or Nursery', 'FA-GC' : 'Golf Course',
-                'FA-HP' : 'Hydroelectric Plant', 'FA-LF' : 'Landfill', 'FA-OF' : 'Outfall',
-                'FA-PV' : 'Pavement', 'FA-QC' : 'Lab or Sample Prep Area', 'FA-SEW' : 'Wastewater Sewer',
-                'FA-SPS' : 'Septic System', 'FA-STS' : 'Storm Sewer', 'FA-TEP' : 'Thermoelectric Plant',
-                'FA-WDS' : 'Water Distribution System', 'FA-WIW' : 'Waste Injection Well',
-                'FA-WTP' : 'Water-supply Treatment Plant', 'FA-WU' : 'Water Use Establishment',
-                'FA-WWD' : 'Wastewater land application', 'FA-WWTP' : 'Wastewater Treatment Plant',
-                'GL' : 'Glacier', 'GW' : 'Well', 'GW-CR' : 'Collector or Ranney type well',
-                'GW-EX' : 'Extensometer Well', 'GW-HZ' : 'Hyporheic-zone Well',
-                'GW-IW' : 'Interconnected Well', 'MW' : 'Multiple Wells', 'GW-TH' : 'Test Hole',
-                'LA' : 'Land', 'LA-EX' : 'Excavation', 'LA-OU' : 'Outcrop', 'LA-PLY' : 'Playa',
-                'LA-SH' : 'Soil Hole', 'LA-SNK' : 'Sinkhole', 'LA-SR' : 'Shore', 'LA-VOL' : 'Volcanic Vent',
-                'LK' : 'Lake', 'OC' : 'Ocean', 'OC-CO' : 'Coastal', 'SB' : 'Subsurface', 'SB-CV' : 'Cave',
-                'SB-GWD' : 'Groundwater Drain', 'SB-TSM' : 'Tunnel', 'SB-UZ' : 'Unsaturated Zone',
-                'SP' : 'Spring', 'SS' : 'Specific Source', 'ST' : 'Stream', 'ST-CA' : 'Canal',
-                'ST-DCH' : 'Ditch', 'ST-TS' : 'Tidal Stream', 'WE' : 'Wetland'}
-
+floodIcons = ['school-15','green_school-15','yellow_school-15','red_school-15']
 # Command Line Arguments:
-parser = argparse.ArgumentParser(description='USGS Station Data Grabber')
-parser.add_argument('fipsCode',
-                    help='County FIPS Codes. May be separated by commas. Ex: 37119,37118')
-parser.add_argument('outFile',
-                    help='File to save GEOJSON output to')
-args=parser.parse_args()
+#parser = argparse.ArgumentParser(description='USGS Station Data Grabber')
+#parser.add_argument('fipsCode',
+#                    help='County FIPS Codes. May be separated by commas. Ex: 37119,37118')
+#parser.add_argument('outFile',
+#                    help='File to save GEOJSON output to')
+#args=parser.parse_args()
+
+# Get flood information...
+colnames = ['nwsID','usgsID','action','minor','moderate','major','unit','name','state']
+data = pandas.read_csv('gageInfo/floodStages.csv',skiprows=5, names=colnames)
+floodID = data.usgsID.tolist()
+floodAction = data.action.tolist()
+floodMinor = data.minor.tolist()
+floodModerate = data.moderate.tolist()
+floodMajor = data.major.tolist()
+floodUnit = data.unit.tolist()
 
 # Grab data from URL...
-response = urllib.request.urlopen("http://waterservices.usgs.gov/nwis/iv/?countyCd="+args.fipsCode)
+#response = urllib.request.urlopen("http://waterservices.usgs.gov/nwis/iv/?countyCd="+args.fipsCode)
+response = urllib.request.urlopen("http://waterservices.usgs.gov/nwis/iv/?countyCd=37119")
 tree = etree.parse(response)
 root = tree.getroot()
 #print(root[:][0][0].text)
 # Prepare outfile
-fileName = open(args.outFile,'w')
-
+#fileName = open(args.outFile,'w')
+fileName = open('test.geojson','w')
 # Prepare to enter main loop...
 i=0 # Counter
 stID=0 # Station ID Counter
@@ -65,28 +55,66 @@ print( '"features": [',file=fileName)
 # Loop through all station reports...
 for child in root:
     if i >= 1 : # First entry is a request summary, so skip that
-
+        # Yup, Variables...
+        # Parameter is arbitrary, different XML call per variable per station
+        # that's why we have to figure out what reading it is in the IF statements
+        # below...
+        stationName = root[i][0][0].text
+        usgsID = root[i][0][1].text
+        stationType = root[i][0][4].text
+        lat = root[i][0][3][0][0].text
+        lon = root[i][0][3][0][1].text
+        parameter = root[i][2][0].text 
+        
 	# Station Information...
-        if tempName != root[i][0][0].text: # Only provide station info if this is a new station...
+        if tempName != stationName: # Only provide station info if this is a new station...
+            j=0
+            floodOption = 0
+            for site in floodID: # Firstly, can this station give us flood info?
+                if str(site).strip() == usgsID:
+                    floodOption = 1
+                    if floodUnit[j] == 'ft':
+                        floodOptHgt = 1
+                        floodPlace = j
+                    elif floodUnit[j] == 'cfs':
+                        floodOptFlo = 1
+                        floodPlace = j
+                    else:
+                        floodOption = 0
+                else:
+                    floodOption = 0
+                j=j+1
+                    
             if i > 1 :
                 stID=stID+1
                 print('}',file=fileName)
                 print( '},',file=fileName)
             print( '  { "type": "Feature",', end='',file=fileName)
-            print( '"geometry": {"type": "Point", "coordinates": [',root[i][0][3][0][1].text,',' ,root[i][0][3][0][0].text,']},',file=fileName)
-            print('"properties": {"stationName": "',root[i][0][0].text,'"', end='',file=fileName)
-            print(',"stationType": "',root[i][0][4].text,'"', end='',file=fileName)
+            print( '"geometry": {"type": "Point", "coordinates": [',lon,',' ,lat,']},',file=fileName)
+            print('"properties": {"stationName": "',stationName,'"', end='',file=fileName)
+            print(',"stationType": "',stationType,'"', end='',file=fileName)
             print( ',"stID":"',stID,'"', end='',file=fileName)
-            print( ',"usgsID":"',root[i][0][1].text,'"',end='',file=fileName)
-            tempName = root[i][0][0].text
+            print( ',"usgsID":"',usgsID,'"',end='',file=fileName)
+            tempName = stationName
 
         # ID Report type and assign appropriate property...        
         if root[i][1][0].get("variableID") == '45807197' :
-            print( ',"streamFlow":"',root[i][2][0].text,'"', end='',file=fileName)
+            print( ',"streamFlow":"',parameter,'"', end='',file=fileName)
+            if floodOption == 1 and floodOptHgt == 1 :
+                stage = usgs.floodStage(parameter,floodPlace,floodAction,floodMinor,floodModerate,floodMajor)
+                print( ',"icon":"',floodIcons[stage],'"', end='',file=fileName)
+            else:
+                print( ',"icon":"',floodIcons[0],'"', end='',file=fileName)
         if root[i][1][0].get("variableID") == '45807202' :
-            print( ',"gageHeight":"',root[i][2][0].text,'"', end='',file=fileName)
+            print( ',"gageHeight":"',parameter,'"', end='',file=fileName)
+            if floodOption == 1 and floodOptHgt == 1 :
+                stage = usgs.floodStage(parameter,floodPlace,floodAction,floodMinor,floodModerate,floodMajor)
+                print( ',"icon":"',floodIcons[stage],'"', end='',file=fileName)
+            else:
+                print( ',"icon":"',floodIcons[0],'"', end='',file=fileName)
         if root[i][1][0].get("variableID") == '45807140' :
-            print( ',"rainfall":"',root[i][2][0].text,'"', end='',file=fileName)
+            print( ',"rainfall":"',parameter,'"', end='',file=fileName)
+            print( ',"icon":"green_water-15"', end='',file=fileName)
 
     i = i+1 # Advance loop counter
 
